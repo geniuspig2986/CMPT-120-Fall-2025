057a3bf4aac720247b1b28c4accc071493b2a6a7
geniuspig2986@gmail.com <geniuspig2986@gmail.com>
2025-09-08 11:57:59 -0700

    First class and Chatbot

---
+++ Basic Chatbot.py
@@ -0,0 +1,14 @@
+# Chatbot
+# Author: Shenghua Jin
+# September 8 2025
+
+# === Algorithm ===
+# Print out a greeting
+print("Hello! I'm a Chatbot.")
+# Get the user's name form input
+print("What's your name?")
+name = input()
+# Cover edge cases
+# Respond nicely
+print("Nice to meet you " + name)
+
